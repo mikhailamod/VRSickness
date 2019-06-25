90cc5385f2aedf6a1236b9d15e73ed32b5b6fad1
Brandon Gower-Winter <bgwsa101@gmail.com>
2019-06-25 14:40:59 +0200

    Added Gate Obstacle

---
+++ VRMove/Assets/Scripts/Obstacles/GateObstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateObstacle : Obstacle
+{
+
+    public Animator animator;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void Trigger()
+    {
+        Debug.Log("Working");
+        animator.SetBool("OpenGate", true);
+    }
+}
---
+++ VRMove/Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Obstacle : MonoBehaviour
+{
+
+    public float proximity;
+    public KeyCode input;
+
+    protected Transform player;
+
+    protected virtual void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (Mathf.Abs(transform.position.z - player.position.z) < proximity && Input.GetKeyDown(input))
+        {
+            Trigger();   
+        }
+    }
+
+    protected abstract void Trigger();
+}
