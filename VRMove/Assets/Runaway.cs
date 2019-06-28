9d3a09a57534b46357b38f68796e6c3ca03c9c4e
Jethro <mlljet002>
2019-06-28 09:05:18 +0200

    Added Animals

---
+++ VRMove/Assets/Runaway.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runaway : MonoBehaviour
+{
+
+public GameObject playerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	float dist = Vector3.Distance(playerObject.transform.position, transform.position);
+	//Debug.Log(dist);
+        if (dist<5){
+		Debug.Log("Time to run");
+		AnimationControl animationControl = GetComponent<AnimationControl>();
+		animationControl.SetAnimation("isWalking");
+	}
+    }
+}
