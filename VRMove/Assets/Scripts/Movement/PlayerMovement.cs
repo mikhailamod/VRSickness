11b511b68739219f3620fad54b3c3209e5de8388
Mikhail Amod <mickeyamod@gmail.com>
2019-06-28 10:28:02 +0200

    Added sound effects

--- VRMove/Assets/Scripts/Movement/PlayerMovement.cs
+++ VRMove/Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public SoundManager soundManager;
+    public string walkType = "walk";
     public void Move(float delta)
     {
         transform.position += new Vector3(0, 0, delta);
+
+        if(!soundManager.isPlaying(walkType))
+        {
+            soundManager.playSound(walkType);
+        }
+    }
+
+    public void setWalkType(string walkType)
+    {
+        this.walkType = walkType;
     }
 }
