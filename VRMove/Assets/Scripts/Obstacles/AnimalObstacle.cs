3a71ebe5f61468611f109cb27ea749268a44dce1
Brandon Gower-Winter <bgwsa101@gmail.com>
2019-07-08 09:30:26 +0200

    Something Something  animal obstacle

---
+++ VRMove/Assets/Scripts/Obstacles/AnimalObstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalObstacle : Obstacle
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void Update()
+    {
+        if (Mathf.Abs(transform.position.z - player.position.z) < proximity)
+        {
+            Trigger();
+        }
+    }
+
+    protected override void Trigger()
+    {
+        animator.SetBool("isWalking", true);
+    }
+}
