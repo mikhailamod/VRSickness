40c9c0da6223cafb18d03c3e29a0a4e775e0c2f3
Mikhail Amod <mickeyamod@gmail.com>
2019-07-12 10:03:45 +0200

    Added rock obstacle

---
+++ VRMove/Assets/Scripts/Obstacles/RockObstacle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockObstacle : Obstacle
+{
+
+    public ParticleSystem particleEffect;
+
+    public string soundEffectKey;
+
+    protected override void Trigger()
+    {
+        hasTriggered = true;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        particleEffect.Play();
+        PlaySound(soundEffectKey);
+    }
+
+    void PlaySound(string key)
+    {
+        if(key.Length > 0)
+        {
+            SoundManager.Instance.PlaySound(key);
+            return;
+        }
+        return;
+    }
+
+}
